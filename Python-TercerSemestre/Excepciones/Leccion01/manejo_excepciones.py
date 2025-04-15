9599aeca87c9a1af5059b9a9328b984f0ed9d5d8
Roberto Jose Lopez <Roberlopez1105@gmail.com>
2025-04-14 21:03:00 -0300

    Update manejo_excepciones.py

--- Python-TercerSemestre/Excepciones/Leccion01/manejo_excepciones.py
+++ Python-TercerSemestre/Excepciones/Leccion01/manejo_excepciones.py
@@ -11,6 +11,10 @@ except ZeroDivisionError as e:
     print(f"ZeroDivisionError - Ocurrio un error: {type(e)}")
 except Exception as e:
     print(F"Excepcion - Ocurrio un error : {type(e)}")
+else:
+    print("No se se arrojo ninguna excepcion")
+finally: #siempre se va a ejecutar
+    print("Ejecucion de este bloque finally")
 
 print(f"Resultado es: {resultado}")
 print("seguimos . . .")
