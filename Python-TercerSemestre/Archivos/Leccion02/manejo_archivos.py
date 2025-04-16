cf6c484b6e70440ce81a7fb3bc8f8806c0c4a30b
Roberto Jose Lopez <Roberlopez1105@gmail.com>
2025-04-16 20:57:42 -0300

    leccion2_video3

--- Python-TercerSemestre/Archivos/Leccion02/manejo_archivos.py
+++ Python-TercerSemestre/Archivos/Leccion02/manejo_archivos.py
@@ -2,8 +2,14 @@
 from logging import exception
 
 try:
-    archivo = open('prueba.txt','w') # La W es de write que significa escribir
+    archivo = open('prueba.txt','w', encoding="utf8") # La W es de write que significa escribir
+    #encoding="utf8" sirve para escribir los acentos
+    archivo.write('Programamos con diferentes tipos de archivos en txt.\n') #\n es un salto de linea
+    archivo.write('Los acentos son importantes para las palabras\n')
+    archivo.write("como por ejemplo: acción, ejecución y producción\n")
+    archivo.write('Con esto terminamos')
 except Exception as e:
     print(e)
 finally: # siempre se ejecuta
     archivo.close() # con esto se debe cerrar el archivo
+#archivo.write("Todo quedo perfecto") esto es un error porque el archivo esta cerrado al ejecutar finally
