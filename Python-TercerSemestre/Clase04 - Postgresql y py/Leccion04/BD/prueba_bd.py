d38de5f7034b034f613367799eefe87d5f5f57a6
Roberto Jose Lopez <Roberlopez1105@gmail.com>
2025-05-12 00:11:39 -0300

    clase05 parte 2 terminada

--- Python-TercerSemestre/Clase04 - Postgresql y py/Leccion04/BD/prueba_bd.py
+++ Python-TercerSemestre/Clase04 - Postgresql y py/Leccion04/BD/prueba_bd.py
@@ -1,17 +1,17 @@
-from logging import exception
-
 import psycopg2 # esto esx para poder conectarnos a Postgre
-conexion = psycopg2.connect(user="postgres",password="admin",host='127.0.0.1',port="5432",database='prueba_bd',)
-try:
-    with conexion:
-        with conexion.cursor() as cursor:
-            sentencia ='SELECT * FROM persona where id_persona = 1'
-            cursor.execute(sentencia) # DE ESTA MANERA EJECUTAMOS LA SETENCIA
-            registros = cursor.fetchall() # RECUPERAMOS TODOS LOS REGISTROS QUE SERAN UNA LISTA
-            print(registros)
+conexion = psycopg2.connect(
+    user="postgres",
+    password="admin",
+    host='127.0.0.1',
+    port="5432",
+    database='prueba_bd',
+)
 
-except Exception as e:
-    print(f"Ocurrio un error{e}")
-finally:
-    conexion.close()
-#https://www.psycopg.org/docs/usage.html
+cursor = conexion.cursor()
+sentencia ='SELECT * FROM persona'
+cursor.execute(sentencia) # DE ESTA MANERA EJECUTAMOS LA SETENCIA
+registros = cursor.fetchall() # RECUPERAMOS TODOS LOS REGISTROS QUE SERAN UNA LISTA
+print(registros)
+
+cursor.close()
+conexion.close()
